4db500b38d60fb78a1f545adea8abc45f48962e0
Setiawan Putra Hendratno <74444281+setiawanph@users.noreply.github.com>
2025-01-10 12:04:41 +0700

    Files of Doorbell Code

---
+++ Code_Alarm_copy_20250110120115.ino
@@ -0,0 +1,48 @@
+#include "discord.h"
+
+#include "pitches.h"
+#define BUZZER_PIN 9
+#define button 2
+
+int melody[] = {
+  NOTE_E5, NOTE_D5, NOTE_FS4, NOTE_GS4,
+  NOTE_CS5, NOTE_B4, NOTE_D4, NOTE_E4,
+  NOTE_B4, NOTE_A4, NOTE_CS4, NOTE_E4,
+  NOTE_A4
+};
+
+int durations[] = {
+  8, 8, 4, 4,
+  8, 8, 4, 4,
+  8, 8, 4, 4,
+  2
+};
+
+//String DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1325332605637496943/VZZy1uo-JqLtltkl_1pDUa4Bh8yFqyh7CxicRaOkXoJImugGQZ5bGRqX_Z7hoqN8YRvP";
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(button, INPUT_PULLUP);
+  pinMode(BUZZER_PIN, OUTPUT);
+  connectWIFI();
+}
+
+void loop() {
+  int size = sizeof(durations)/sizeof(int);
+  byte state = digitalRead(button);
+  Serial.println(state);
+  //delay(100);
+  
+  if(state != 1){
+    for (int note = 0; note < size; note++){
+      int duration = 1000/durations[note];
+      tone(BUZZER_PIN, melody[note], duration);
+
+      int pauseBetweenNotes = duration * 1.30;
+      delay(pauseBetweenNotes);
+
+      noTone(BUZZER_PIN);
+    }
+    discord_send("Hey! You got a package.");
+  }
+}
